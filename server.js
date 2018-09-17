6c5a4f8b8b9bb4c55da8113df22995811943f091
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-09-16 23:54:41 -0400

    updated server file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var exphbs  = require("express-handlebars");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Our scraping tools
@@ -9,3 +10,33 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 // Makes HTTP request for HTML page
 var request = require("request");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Middleware
+
+// Use morgan logger for logging the requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true}));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port" + PORT + "!");
+});
