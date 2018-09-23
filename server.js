6980b2c3a36ca334c0254b97018633d18b5bfb85
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-09-23 17:00:27 -0400

    make it better

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var methodOverride = require('method-override');
 
-// Port changed to 5000
-var PORT = 5000;
+// Port changed to 3000
+const PORT = process.env.PORT || 3000;
 
 // Initialize Express app
 var app = express();
